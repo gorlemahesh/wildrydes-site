bde71fd8c7b5cc43b93b2affd90f08050a38e0f4
gorlemahesh <maheshgorle12345@gmail.com>
2025-04-16 15:22:50 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_Cbcs2IhdZ', //
-        userPoolClientId: '4flcor11e6858629m3pr50moo3', //
+        userPoolClientId: '27p57bin3p63rjnma2s7ne34c9', //
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
